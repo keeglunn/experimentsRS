e3269a0921c80ef583f8130d47cc809e4e37025b
Keegan <keeglunn@gmail.com>
2024-01-08 19:07:30 -0600

    Working crunchify

--- examples/crunchify/crunchify_1.rs
+++ examples/crunchify/crunchify_1.rs
@@ -5,39 +5,44 @@ use sketches::midi;
 use sketches::midi::twister::constants as twister_constants;
 
 use midir::MidiInputConnection;
-use nannou::prelude::*;
+use nannou::{prelude::*, image::{GenericImage, Pixel}, glam::bool};
 use std::{
     sync::mpsc::{channel, Receiver},
-    u8,
+    u8, time::Duration,
 };
 use wmidi::MidiMessage;
+use nannou::image;
+use nannou::image::GenericImageView;
 
 fn main() {
-    nannou::app(model).update(update)..run();
+    nannou::app(model).update(update).run();
 }
 
 struct Model {
+    image: image::DynamicImage,
+    frame: image::DynamicImage, 
     texture: wgpu::Texture,
     show_frame_count: bool,
-    speeds: [f32; 4],
-    rad_positions: [f32; 4],
+    speed: f32,
+    red_w: f32,
+    green_w: f32,
+    blue_w: f32,
+    last_update: Duration,
     _connection: Option<MidiInputConnection<()>>,
     receiver: Receiver<Vec<u8>>,
 }
 
 fn model(app: &App) -> Model {
 
-    // Load image
     let assets = app.assets_path().unwrap();
     let img_path = assets
-        .join("images")
-        .join("generative_examples")
-        .join("p_4_0_01.jpg");
-    let texture = wgpu::Texture::from_path(app, img_path).unwrap();
+        .join("sunset.png");
+
+    let image = image::open(img_path).unwrap();
 
     let _window = app
         .new_window()
-        .size(1024, 1024) // set window size to image dimensions
+        .size(image.width(), image.height()) // set window size to image dimensions
         .view(view)
         .key_pressed(key_pressed)
         .build()
@@ -46,10 +51,16 @@ fn model(app: &App) -> Model {
     let (tx, rx) = channel();
 
     Model {
-        texture,
+        frame: image.clone(),
+        texture: wgpu::Texture::from_image(app, &image),
+        image,
 
-        speeds: [1.0, 1.0, 1.0, 1.0],
-        rad_positions: [0.0, 0.0, 0.0, 0.0],
+        speed: 1.0,
+        last_update: Duration::new(0, 0),
+        
+        red_w: 1.0,
+        green_w: 1.0,
+        blue_w: 1.0,
 
         _connection: midi::init(tx),
         receiver: rx,
@@ -57,15 +68,81 @@ fn model(app: &App) -> Model {
     }
 }
 
-fn update(_app: &App, model: &mut Model, update: Update) {
-    update_speeds(model);
-    update_positions(model, update.since_last)
+fn update(app: &App, model: &mut Model, update: Update) {
+    update_params(model);
+
+    if (update.since_start - model.last_update > Duration::from_millis((250.0 / model.speed).trunc() as u64)) {
+//        println!("Running update_image");
+        model.last_update = update.since_start;
+        update_image(model);
+        
+        model.texture = wgpu::Texture::from_image(app, &model.frame);
+    }
+}
+
+fn update_params(model: &mut Model) {
+    for data in model.receiver.try_iter() {
+        match MidiMessage::try_from(data.as_slice()) {
+            Err(e) => {
+                eprint!("Invalid midi message {:?}\n", e);
+            }
+            Ok(midi_message) => match midi_message {
+                wmidi::MidiMessage::ControlChange(channel, note, velocity) => {
+                    match (channel, note) {
+                        (wmidi::Channel::Ch1, twister_constants::ZERO_ZERO) => {
+                            let v: u8 = velocity.into();
+                            model.speed = sketches::util::speed_func(v as f32) 
+                        }
+                        (wmidi::Channel::Ch1, twister_constants::ONE_ZERO) => {
+                            let v: u8 = velocity.into();
+                            model.red_w = sketches::util::speed_func(v as f32) 
+                        }
+                        (wmidi::Channel::Ch1, twister_constants::ONE_ONE) => {
+                            let v: u8 = velocity.into();
+                            model.green_w = sketches::util::speed_func(v as f32) 
+                        }
+                        (wmidi::Channel::Ch1, twister_constants::ONE_TWO) => {
+                            let v: u8 = velocity.into();
+                            model.blue_w = sketches::util::speed_func(v as f32) 
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            },
+        };
+    }
+}
+
+fn update_image(model: &mut Model) {
+    let (w, h) = model.image.dimensions();
+    for grid_x in 0..w {
+        for grid_y in 0..h {
+            let image_pixel = model.image.get_pixel(grid_x, grid_y);
+
+            let red = model.red_w * (image_pixel[0] as f32 / 255.0);
+            let green = model.green_w * (image_pixel[1] as f32 / 255.0);
+            let blue = model.blue_w * (image_pixel[2] as f32 / 255.0);
+            
+            let rand_pick: f32 = random_range(0.0, 1.0);
+            // Give each color a win chance based on their relative value. black wins if no color wins
+
+            if (rand_pick < red) {
+                model.frame.put_pixel(grid_x, grid_y, Pixel::from_channels(255, 0, 0, 0));
+            } else if (rand_pick < red + green) {
+                model.frame.put_pixel(grid_x, grid_y, Pixel::from_channels(0, 255, 0, 0));
+            } else if (rand_pick < red + green + blue) {
+                model.frame.put_pixel(grid_x, grid_y, Pixel::from_channels(0, 0, 255, 0));
+            } else {
+                model.frame.put_pixel(grid_x, grid_y, Pixel::from_channels(0, 0, 0, 0));
+            }
+        }
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let win = app.window_rect();
-    let big_radius: f32 = win.w() / 3.0;
 
     draw.background().color(BLACK);
 
@@ -73,17 +150,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         draw_frame_count(&frame, &draw, &win);
     }
 
-    for i in 0..model.rad_positions.len() {
-        let x = (model.rad_positions[i]).sin() * (big_radius * ((1.0 + i as f32) * 0.25));
-        let y = (model.rad_positions[i]).cos() * (big_radius * ((1.0 + i as f32) * 0.25));
- 
-        draw.ellipse()
-            .x_y(x, y)
-            .radius(30 as f32)
-            .stroke(WHITE)
-            .stroke_weight(2 as f32)
-            .color(BLACK);
-    }
+    draw.texture(&model.texture);
 
     // Write to the window frame.
     draw.to_frame(app, &frame).unwrap();
@@ -107,45 +174,3 @@ fn draw_frame_count(frame: &Frame, draw: &Draw, win: &Rect) {
         .color(WHITE);
 }
 
-fn update_speeds(model: &mut Model) {
-    for data in model.receiver.try_iter() {
-        match MidiMessage::try_from(data.as_slice()) {
-            Err(e) => {
-                eprint!("Invalid midi message {:?}\n", e);
-            }
-            Ok(midi_message) => match midi_message {
-                wmidi::MidiMessage::ControlChange(channel, note, velocity) => {
-                    match (channel, note) {
-                        (wmidi::Channel::Ch1, twister_constants::ZERO_ZERO) => {
-                            let v: u8 = velocity.into();
-                            model.speeds[0] = 1.0 + ((v as f32) / 127.0) * 10.0
-                        }
-                        (wmidi::Channel::Ch1, twister_constants::ZERO_ONE) => {
-                            let v: u8 = velocity.into();
-                            model.speeds[1] = 1.0 + ((v as f32) / 127.0) * 10.0
-                        }
-                        (wmidi::Channel::Ch1, twister_constants::ZERO_TWO) => {
-                            let v: u8 = velocity.into();
-                            model.speeds[2] = 1.0 + ((v as f32) / 127.0) * 10.0
-                        }
-                        (wmidi::Channel::Ch1, twister_constants::ZERO_THREE) => {
-                            let v: u8 = velocity.into();
-                            model.speeds[3] = 1.0 + ((v as f32) / 127.0) * 10.0
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
-            },
-        };
-    }
-}
-
-const MOV_PER_SEC: f32 = 1.0;
-
-fn update_positions(model: &mut Model, update_time: std::time::Duration) {
-    for i in 0..model.rad_positions.len() {
-        let update_len = (update_time.as_secs_f32() * MOV_PER_SEC);
-        model.rad_positions[i] += update_len * model.speeds[i]
-    }
-}
